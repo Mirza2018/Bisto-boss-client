2208509663054b7f5912cb0a21c9552adcaec48f
unknown <mirza.eee.4th@gmail.com>
2023-09-30 20:08:35 +0600

    Category

---
+++ src/Pages/Home/PopulerMenu/PopulerMenu.jsx
@@ -0,0 +1,16 @@
+import SectionTitle from "../../../components/SectionTitle/SectionTitle";
+
+
+const PopulerMenu = () => {
+    return (
+        <div>
+            <SectionTitle
+                subHeading={'Chech it Out'}
+                heading={'From our menu'}
+            ></SectionTitle>
+
+        </div>
+    );
+};
+
+export default PopulerMenu;
--- src/components/SectionTitle/SectionTitle.jsx
+++ src/components/SectionTitle/SectionTitle.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 const SectionTitle = ({ subHeading, heading }) => {
     return (
-        <div>
-            <p className='text-yellow-600'>---{subHeading}---</p>
-            <h1 className='text-3xl'>{heading}</h1>
+        <div className='mx-auto my-10 w-3/12 text-center'>
+            <p className='text-yellow-600 pb-3 '>---{subHeading}---</p>
+            <h1 className='text-4xl uppercase  border-y-4 py-4'>{heading}</h1>
         </div>
     );
 };
